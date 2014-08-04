578ed62113ad874067e0d44c6771c52f46597f7c
schlager <zarquon42@gmail.com>
2014-08-04 12:07:11 +0200

    added createAntsArgs

--- R/newAntsExample.r
+++ R/newAntsExample.r
@@ -31,6 +31,7 @@ newAntsExample <- function(reference, target, setting=c("fast","production"),itk
 }
 
 
+
 #' calls antsApplyTransformsToPoints
 #'
 #' calls antsApplyTransformsToPoints
@@ -60,11 +61,54 @@ antsTransformPoints.mesh3d <- function(mat,affine,warp,antsdir="~/GIT/DEV/ANTSbu
     mesh <- mat
     mat <- vert2points(mat)
     pts <- mat%*% diag(c(-1,-1,1))
-    write.csv(pts,file="pts.csv",row.names=F)
-    cmd <- paste0(antsdir,"antsApplyTransformsToPoints -d 3 -i pts.csv -o ptsDeformed2.csv -t ",affine," -t ",warp)
+    ptsname <- paste0(tempdir(),"/pts.csv")
+    write.csv(pts,file=ptsname,row.names=F)
+    output <- paste0(tempdir(),"/ptsDeformed2.csv")
+    
+    cmd <- paste0(antsdir,"antsApplyTransformsToPoints -d 3 -i ",ptsname," -o ",output," -t ",affine," -t ",warp)
     print(cmd)
     system(cmd)
-    readit <- as.matrix(read.csv("ptsDeformed2.csv")[,1:3])%*%diag(c(-1,-1,1))##convert back to RAS space
+    readit <- as.matrix(read.csv(output)[,1:3])%*%diag(c(-1,-1,1))##convert back to RAS space
     mesh$vb[1:3,] <- t(readit)
-    return(mesh)
+return(mesh)
 }
+#' interface to generate arguments for antsRegistration command
+#'
+#' interface to generate arguments for antsRegistration command
+#'
+#' @param reference path to moving image
+#' @param target path to fixed image
+#' @param setting name of the setting to be attached to output
+#' @param percent parameter for ants
+#' @param its parameters for non-syn registration
+#' @param synargs parameters for syn registration
+#' @param itkthreads integer: specify number of threads to be used
+#' @param trans run translation
+#' @param rigid run rigid matching
+#' @param affine run affine matching
+#' @param syn run syn matching
+#' @export
+createAntsArgs <- function(reference,target,setting="custom",percent=0.1,its="10000x10000x10000",synargs ="[100x10x1,0,5]",cc=FALSE,itkthreads=4,trans=T,rigid=T,affine=T,syn=T) {
+    Sys.setenv(ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=itkthreads)
+    nm=paste0(target,"_fixed_",reference,"_moving_",setting[1])
+     antsargs <- list(d=3)
+    if (trans)
+              antsargs <- append(antsargs,list(m=paste0("mattes[",target,",",reference,",1,32,regular,",percent,"]"), t="translation[0.1]", c=paste0("[",its,",1e-8,20]"), s="4x2x1vox", f="6x4x2",l="1"))
+                                 ## translation step
+    if (rigid)
+        antsargs <- append(antsargs,list(
+                     m=paste0("mattes[",target,",",reference,",1,32,regular,",percent,"]"), t="rigid[0.1]",c=paste0("[",its,",1e-8,20]"), s="4x2x1vox", f="3x2x1"))
+    ##rigid alignment
+    if (affine)
+           antsargs <- append(antsargs,list(                 
+               m=paste0("mattes[",target,",",reference,",1,32,regular,",percent,"]"), t="affine[0.1]",c=paste0("[",its,",1e-8,20]"), s="4x2x1vox",  f="3x2x1"))
+    if (syn)
+        antsargs <- append(antsargs,list(         
+                     m=paste0("mattes[",target,",",reference,",0.5,32]"),m=paste0("cc[",target,",",reference,",0.5,4]"),t="SyN[0.2,3,0]", c=synargs,  s="1x0.5x0vox", f="4x2x1"))
+    antsargs <- append(antsargs,list(u="1",l="1",z="1",float="0",
+                                     o=paste0("[",nm,",",nm,"_diff.nii.gz,",nm,"_inv.nii.gz]")))
+     return(antsargs)
+}
+
+
+
