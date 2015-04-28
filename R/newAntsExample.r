09dc1396c6f0db6bd7c119ff107f7e3f9ba85359
Stefan Schlager <zarquon42@gmail.com>
2015-04-28 09:27:45 +0200

    made interface of createAntsArgs more comprehensive

--- R/newAntsExample.r
+++ R/newAntsExample.r
@@ -90,68 +90,71 @@ return(mesh)
 #' @param searchradius searchradius for CC metric
 #' @param mattesbins binsize for Mattes MI metric
 #' @param its parameters for non-syn registration
-#' @param synargs parameters for syn registration
-#' @param itkthreads integer: specify number of threads to be used
-#' @param trans run translation
-#' @param rigid run rigid matching
-#' @param affine run affine matching
-#' @param syn run syn matching
-#' @param BSplineDisp use B-Spline deformations
-#' @param gauss use gaussian Smoothed deformation fields
-#' @param expo use Exponential deformation type
-#' @param mattesweight weight to attribute to mattes metric
-#' @param ccweight weight to attribute to CC metric
+#' @param synargs character: convergence valzes MxNxO [MxNxO,<convergenceThreshold=1e-6>,<convergenceWindowSize=10>]
+#' @param affine character vector of non-elastic transform to use
+#' @param elastic character vector of elastic procedures (including parameters). E.g "SyN[0.2,3,0]"
+#' @param mattesweight weight to attribute to mattes metric (used only in elastic registration)
+#' @param ccweight weight to attribute to CC metric (used only in elastic registration)
 #' @param dims dimensionality of data
+#' @param elasticS smoothing sigmas (see command args of antsRegistration)
+#' @param elasticF shrink factors (see command args of antsRegistration)
 #' @param initTransform run initial coarse alignment. These features include using the geometric center of the
 #' images (=0), the image intensities (=1), or the origin of the images (=2).
-#' @param PSE not supported yet
+#' @param itkthreads integer: specify number of threads to be used
 #' @return returns a list with
 #' \item{antsargs}{arguments passed to antsRegistration}
 #' \item{outname}{basename of outputname}
 #' @export
-createAntsArgs <- function(reference,target,setting="custom",percent=0.1,synpercent=0.1,searchradius=4,mattesbins=32,its="10000x10000x10000",synargs ="[100x10x1,0,5]",cc=FALSE,itkthreads=parallel::detectCores(),trans=T,rigid=T,similarity=T,affine=T,BSplineDisp=F,gauss=F,expo=F,syn=T,mattesweight=0.5,ccweight=0.5,dims=3,initTransform=NULL,PSE=NULL) {
+createAntsArgs <- function(reference,target,setting="custom",percent=0.1,synpercent=0.1,searchradius=4,mattesbins=32,its="10000x10000x10000",synargs ="[100x10x1,0,5]", affine=c("trans","rigid","similarity","affine"),elastic="SyN[0.2,3,0]",mattesweight=0.5,ccweight=0.5,dims=3,initTransform=NULL,elasticS="3x1x0vox",elasticF="3x2x1",itkthreads=parallel::detectCores()) {
+    if (nargs() == 0) {
+        antsRegistration()
+        return()
+    }
     Sys.setenv(ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=itkthreads)
     nm=paste0(target,"_fixed_",reference,"_moving_",setting[1])
-     antsargs <- list(d=dims)
+    weights <- c(mattesweight,ccweight)
+    weights <- weights/sum(weights)
+    mattesweight <- weights[1]
+    ccweight <- weights[2]
+    antsargs <- list(d=dims)
+    affinedef <- c("translation","rigid","similarity","affine")
     if (!is.null(initTransform))
         antsargs <- append(antsargs,list(r=paste0("[",target,",",reference,",",initTransform,"]")))
-    if (trans)
-        antsargs <- append(antsargs,list(m=paste0("mattes[",target,",",reference,",1,",mattesbins,",regular,",percent,"]"), t="translation[0.1]", c=paste0("[",its,",1e-8,20]"), s="4x2x1vox", f="6x4x2",l="1"))
-                                 ## translation step
-    if (similarity)
-        antsargs <- append(antsargs,list(
-                     m=paste0("mattes[",target,",",reference,",1,",mattesbins,",regular,",percent,"]"), t="similarity[0.1]",c=paste0("[",its,",1e-8,20]"), s="4x2x1vox", f="3x2x1"))
-    ##rigid alignment
-    if (rigid)
-        antsargs <- append(antsargs,list(
-                     m=paste0("mattes[",target,",",reference,",1,",mattesbins,",regular,",percent,"]"), t="rigid[0.1]",c=paste0("[",its,",1e-8,20]"), s="4x2x1vox", f="3x2x1"))
-    ##rigid alignment
-    if (affine)
-           antsargs <- append(antsargs,list(                 
-               m=paste0("mattes[",target,",",reference,",1,",mattesbins,",regular,",percent,"]"), t="affine[0.1]",c=paste0("[",its,",1e-8,20]"), s="4x2x1vox",  f="3x2x1"))
     
-    if (gauss)
-        antsargs <- append(antsargs,list(         
-                     m=paste0("mattes[",target,",",reference,",0.5,", mattesbins,",regular,",synpercent,"]"),m=paste0("cc[",target,",",reference,",0.5,", searchradius, "]"),t="GaussianDisplacementField[0.2,3,0]", c=synargs,  s="1x0.5x0vox", f="4x2x1"))
-
-    if (syn)
-        antsargs <- append(antsargs,list(         
-            m=paste0("mattes[",target,",",reference,",",mattesweight,",",mattesbins,",regular,",synpercent,"]"),m=paste0("cc[",target,",",reference,",",ccweight ,",", searchradius, "]"),t="SyN[0.2,3,0]", c=synargs,  s="1x0.5x0vox", f="4x2x1"))
-
-    if (expo)
-        antsargs <- append(antsargs,list(         
-            m=paste0("mattes[",target,",",reference,",0.5,32]"),m=paste0("cc[",target,",",reference,",0.5,4]"),t="Exponential[0.2,3,0.5,10]", c=synargs,  s="1x0.5x0vox", f="4x2x1"))
-    if (BSplineDisp)
-        antsargs <- append(antsargs,list(         
-            m=paste0("mattes[",target,",",reference,",0.5,32]"),m=paste0("cc[",target,",",reference,",0.5,4]"),t="BSplineDisplacementField[0.2,3,0.5,3]", c=synargs,  s="1x0.5x0vox", f="4x2x1"))
-    
-    antsargs <- append(antsargs,list(u="1",l="1",z="1",float="0",
+    affineprefix <- list(m=paste0("mattes[",target,",",reference,",1,",mattesbins,",regular,",percent,"]"))
+    affinesuffix <- list(c=paste0("[",its,",1e-8,20]"), s="4x2x1vox", f="6x4x2",l="1")
+    for (i in affine) {
+        curaff <- match.arg(i,affinedef)
+        curargs <- append(affineprefix,list(t=paste0(curaff,"[0.1]")))
+        curargs <- append(curargs,affinesuffix)
+        antsargs <- append(antsargs,curargs)
+                          
+    }
+    if (length(elastic)) {
+        elasticargs <- list()
+        if (mattesweight > 0)
+            elasticargs <- append(elasticargs,list(m=paste0("mattes[",target,",",reference,",0.5,", mattesbins,",regular,",synpercent,"]")))
+        if (ccweight > 0 )
+            elasticargs <- append(elasticargs,list(m=paste0("cc[",target,",",reference,",0.5,", searchradius, "]")))
+        elasticsuffix <- list(c=synargs,s=elasticS,f=elasticF)
+        for (i in elastic) {
+            elmid <- list(t=i)
+            antsargs <- append(antsargs,unlist(list(elasticargs,elmid,elasticsuffix)))
+        }
+    }    
+    antsargs <- append(antsargs,list(u="1",l="1",z="1",float="1",
                                      o=paste0("[",nm,",",nm,"_diff.nii.gz,",nm,"_inv.nii.gz]")))
     
-        
-     return(list(antsargs=antsargs,outname=basename(nm)))
+    return(list(antsargs=antsargs,outname=basename(nm)))
 }
 
 
 
 
+cmdargs <- function(antsargs) {
+    x <- antsargs$antsargs
+    out <- paste0("-",names(x)," ",x)
+    out <- gsub("-float","--float",out)
+    cat(out)
+    cat("\n")
+}
