4da6c6e5afca541b71d9e7cfb7b09411bbd0e674
Stefan Schlager <zarquon42@gmail.com>
2015-05-04 17:20:02 +0200

    more improvements and docu in createAntsArgs

--- R/createAntsArgs.r
+++ R/createAntsArgs.r
@@ -5,13 +5,12 @@
 #' @param reference path to moving image
 #' @param target path to fixed image
 #' @param setting name of the setting to be attached to output
-#' @param percent parameter for ants
-#' @param searchradius searchradius for CC metric
-#' @param mattesbins binsize for Mattes MI metric
-#' @param its parameters for non-syn registration
-#' @param synargs character: convergence valzes MxNxO [MxNxO,<convergenceThreshold=1e-6>,<convergenceWindowSize=10>]
+#' @param percent subsampling for affine transforms
 #' @param affine character vector of non-elastic transform to use
+#' @param affinereach  character vector assigning the bins for Mattes metric used for affine transforms.
+#' @param affineconvergence character: convergence values for affine transforms MxNxO [MxNxO,<convergenceThreshold=1e-6>,<convergenceWindowSize=10>]
 #' @param elastic character vector of elastic procedures (including parameters). E.g "SyN[0.2,3,0]"
+#' @param elasticconvergence character: convergence valzes MxNxO [MxNxO,<convergenceThreshold=1e-6>,<convergenceWindowSize=10>]
 #' @param elasticmetrics character vector containing the metrics to be used in elastic matching
 #' @param metricweights  character vector of same length as \code{elasticmetrics}, assigning weights to each metric. If NULL, all selected metrics will be weighted equally.
 #' @param metricreach  character vector of same length as \code{elasticmetrics}, assigning the radius/bins for each metric to consider.
@@ -21,18 +20,22 @@
 #' @param initTransform run initial coarse alignment. These features include using the geometric center of the
 #' images (=0), the image intensities (=1), or the origin of the images (=2).
 #' @param itkthreads integer: specify number of threads to be used
+#' @param folder character defining the path where to store the transforms
+#' @param transimg logical: if TRUE the *diff and *inv will be created.
 #' @return returns a list with
 #' \item{antsargs}{arguments passed to antsRegistration}
 #' \item{outname}{basename of outputname}
 #' \item{transforms}{list containing characters naming the transforms}
 #' @export
-createAntsArgs <- function(reference,target,setting="custom",percent=0.1,synpercent=0.1,searchradius=4,mattesbins=32,its="10000x10000x10000",synargs ="[100x10x1,0,5]", affine=c("trans","rigid","similarity","affine"),elastic="SyN[0.2,3,0]",elasticmetrics=c("mattes","cc"),metricweights=NULL, metricreach=c(32,4),dims=3,initTransform=NULL,elasticS="3x1x0vox",elasticF="3x2x1",itkthreads=parallel::detectCores()) {
+createAntsArgs <- function(reference,target,setting="custom",percent=0.1, affine=c("trans","rigid","similarity","affine"),affinereach=32,affineconverge="[10000x10000x10000,1e-8,20]",elastic="SyN[0.2,3,0]",elasticpercent=0.1,elasticconverge ="[100x10x1,0,5]",elasticmetrics=c("mattes","cc"),metricweights=NULL, metricreach=c(32,4),dims=3,elasticS="3x1x0vox",elasticF="3x2x1",initTransform=NULL,itkthreads=parallel::detectCores(),masks=NULL,folder=NULL,transimg=TRUE) {
     if (nargs() == 0) {
         antsRegistration()
         return()
     }
+    if (!is.null(folder))
+        folder <- paste0(folder,"/")
     Sys.setenv(ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=itkthreads)
-    nm=paste0(target,"_fixed_",reference,"_moving_",setting[1])
+    nm <- paste0(folder,basename(target),"_fixed_",basename(reference),"_moving_",setting[1])
     if (is.null(metricweights))
         metricweights <- rep(1,length(elasticmetrics))
     mweights <- metricweights
@@ -46,8 +49,8 @@ createAntsArgs <- function(reference,target,setting="custom",percent=0.1,synperc
     if (!is.null(initTransform))
         antsargs <- append(antsargs,list(r=paste0("[",target,",",reference,",",initTransform,"]")))
     
-    affineprefix <- list(m=paste0("mattes[",target,",",reference,",1,",mattesbins,",regular,",percent,"]"))
-    affinesuffix <- list(c=paste0("[",its,",1e-8,20]"), s="4x2x1vox", f="6x4x2",l="1")
+    affineprefix <- list(m=paste0("mattes[",target,",",reference,",1,",affinereach,",regular,",percent,"]"))
+    affinesuffix <- list(c=affineconverge, s="4x2x1vox", f="6x4x2",l="1")
     for (i in affine) {
         curaff <- match.arg(i,affinedef)
         curargs <- append(affineprefix,list(t=paste0(curaff,"[0.1]")))
@@ -59,22 +62,31 @@ createAntsArgs <- function(reference,target,setting="custom",percent=0.1,synperc
         elasticargs <- list()
         for( i in 1 : length(mweights)) {
             if (mweights[i] != 0) {
-                elasticargs <- append(elasticargs,list(m=paste0(metrics[i],"[",target,",",reference,",",mweights[i],",", metricreach[i],",regular,",synpercent,"]")))
+                elasticargs <- append(elasticargs,list(m=paste0(metrics[i],"[",target,",",reference,",",mweights[i],",", metricreach[i],",regular,",elasticpercent,"]")))
             }
         }
-        elasticsuffix <- list(c=synargs,s=elasticS,f=elasticF)
+        elasticsuffix <- list(c=elasticconverge,s=elasticS,f=elasticF)
         for (i in elastic) {
             elmid <- list(t=i)
             antsargs <- append(antsargs,unlist(list(elasticargs,elmid,elasticsuffix)))
         }
-    }    
-    antsargs <- append(antsargs,list(u="1",l="1",z="1",float="1",
-                                     o=paste0("[",nm,",",nm,"_diff.nii.gz,",nm,"_inv.nii.gz]")))
+    }
+    if (length(masks))
+        antsargs <- append(antsargs,list(x=masks))
+    if (transimg)
+        output <- list(o=paste0("[",nm,",",nm,"_diff.nii.gz,",nm,"_inv.nii.gz]"))
+    else
+        output <- list(o=paste0("[",nm,"]"))
+    antsargs <- append(antsargs,list(u="1",l="1",z="1",float="1"))
+    antsargs <- append(antsargs,output)
 
     transforms <- list()
-    transforms$warpfwd <- paste0(nm,"1Warp.nii.gz")
-    transforms$warpinv <- paste0(nm,"1InverseWarp.nii.gz")
-    transforms$affine <-  paste0(nm,"0GenericAffine.mat")
+    if (length(elastic)) {
+        transforms$warpfwd <- paste0(nm,"1Warp.nii.gz")
+        transforms$warpinv <- paste0(nm,"1InverseWarp.nii.gz")
+    }
+    if (length(affine))
+        transforms$affine <-  paste0(nm,"0GenericAffine.mat")
     return(list(antsargs=antsargs,outname=basename(nm),transforms=transforms))
     
 }
