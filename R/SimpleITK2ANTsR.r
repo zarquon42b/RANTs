630de140cdd6e0d75ac65a16812043c9a3a2abf6
Stefan Schlager <zarquon42@gmail.com>
2015-05-22 09:59:03 +0200

    added simpleitk to antsimage wrappers

---
+++ R/SimpleITK2ANTsR.r
@@ -0,0 +1,45 @@
+#' convert an  SimpleITK image to an antsImage
+#'
+#' convert an  SimpleITK image to an antsImage
+#' @param image object of class SimpleITK
+#' @return returns a antsImage
+#' @export
+sitk2antsImage <- function(image) {
+    if (!require(SimpleITK))
+        stop("please install SimpleITK R-package")
+    pixeltype <- image$GetPixelIDTypeAsString()
+    pitype <- "float"
+    if (grepl("unsigned integer",pixeltype))
+        pitype <- "unsigned int"
+    if (grepl("unsigned char",pixeltype))
+        pitype <- "unsigned char"
+    arr <- as.array(image)
+    
+    aImage <- as.antsImage(arr,spacing=image$GetSpacing(),origin=image$GetOrigin(),pixeltype=pitype)
+    return(aImage)
+}
+
+#' convert an antsImage to a SimpleITK image
+#'
+#' convert an antsImage to a SimpleITK image
+#' @param image object of class antsImage
+#' @return returns a SimpleITK image
+#' @export
+antsImage2sitk <- function(image) {
+    if (!require(SimpleITK))
+        stop("please install SimpleITK R-package")
+    pixeltype <- image@pixeltype
+    arr <- ANTsR::as.array(image)
+    ci <- CastImageFilter()
+    if (grepl("unsigned", pixeltype)) {
+        storage.mode(arr) <- "integer"
+        ci$SetOutputPixelType("sitkUInt16")
+    } else
+        ci$SetOutputPixelType("sitkFloat32")
+    
+    sitkImage <- SimpleITK::as.image(arr,spacing=antsGetSpacing(image),origin=antsGetOrigin(image))
+    sitkImage$SetDirection(as.vector(t(antsGetDirection(image))))
+    sitkImage <- ci$Execute(sitkImage)
+    
+    return(sitkImage)
+}
